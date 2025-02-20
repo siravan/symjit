d94737bc23718b5ad445b4a8ba98f08fde6a6f57
Shahriar Iravanian <siravan@svtsim.com>
2025-02-20 16:37:20 -0500

    darwin target added

--- python/symjit/engine.py
+++ python/symjit/engine.py
@@ -18,6 +18,8 @@ if sys.platform == "linux" and platform.machine() == "x86_64":
     dll_name = find_dll("x86_64-linux")
 if sys.platform == "linux" and platform.machine() == "aarch64":
     dll_name = find_dll("aarch64-linux")
+if sys.platform == "darwin" and platform.machine() == "x86_64":
+    dll_name = find_dll("darwin")
 elif sys.platform == "win32":
     dll_name = find_dll("win_amd64")
 
