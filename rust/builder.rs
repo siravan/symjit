7a819914b4d4eb61685e127014e7f59946ca4b4d
Shahriar Iravanian <siravan@svtsim.com>
2026-06-04 15:05:00 -0400

    fixing sum/prod loop

--- rust/builder.rs
+++ rust/builder.rs
@@ -171,16 +171,9 @@ impl Builder {
         let one = self.create_const(1.0)?;
         let q = self.create_binary("plus", var.clone(), one.clone())?;
         self.add_assign(var.clone(), q)?;
-        let cond = self.create_binary("gt", var, end)?;
-
-        // note: we need this `and` to convert `true` (all-1 mask, NaN) to
-        // a regular floating point number
-        let cond2 = self.create_binary("and", cond, one)?;
-
+        let cond = self.create_binary("leq", var, end)?;
         let label = format!(".L{}", loop_id);
-
-        // jump to label if `cond2 == false`
-        self.block().add_branch_if(cond2, &label, false);
+        self.block().add_branch_if(cond, &label, true);
 
         Ok(accum_var)
     }
