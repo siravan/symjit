8e4e57b8b1254dc81e48af877014feca121a7815
Shahriar Iravanian <siravan@svtsim.com>
2026-07-08 10:53:44 -0400

    ver 2.19.4; arm64 paired complex load/save

--- rust/arm/vector.rs
+++ rust/arm/vector.rs
@@ -257,15 +257,15 @@ impl Generator for ArmSimdGenerator {
     }
 
     fn load_mem_complex(&mut self, xd: Reg, yd: Reg, idx: u32) {
-        // self.load_mem(xd, idx);
-        // self.load_mem(yd, idx + 1);
-        self.load_paired_q_from_mem(ϕ(xd), ϕ(yd), MEM, idx);
+        self.load_mem(xd, idx);
+        self.load_mem(yd, idx + 1);
+        // self.load_paired_q_from_mem(ϕ(xd), ϕ(yd), MEM, idx);
     }
 
     fn save_mem_complex(&mut self, xs: Reg, ys: Reg, idx: u32) {
-        // self.save_mem(xs, idx);
-        // self.save_mem(ys, idx + 1);
-        self.save_paired_q_to_mem(ϕ(xs), ϕ(ys), MEM, idx);
+        self.save_mem(xs, idx);
+        self.save_mem(ys, idx + 1);
+        // self.save_paired_q_to_mem(ϕ(xs), ϕ(ys), MEM, idx);
     }
 
     fn load_param_complex(&mut self, xd: Reg, yd: Reg, idx: u32) {
@@ -274,15 +274,15 @@ impl Generator for ArmSimdGenerator {
     }
 
     fn load_stack_complex(&mut self, xd: Reg, yd: Reg, idx: u32) {
-        // self.load_stack(xd, idx);
-        // self.load_stack(yd, idx + 1);
-        self.load_paired_q_from_mem(ϕ(xd), ϕ(yd), SP, idx);
+        self.load_stack(xd, idx);
+        self.load_stack(yd, idx + 1);
+        // self.load_paired_q_from_mem(ϕ(xd), ϕ(yd), SP, idx);
     }
 
     fn save_stack_complex(&mut self, xs: Reg, ys: Reg, idx: u32) {
-        // self.save_stack(xs, idx);
-        // self.save_stack(ys, idx + 1);
-        self.save_paired_q_to_mem(ϕ(xs), ϕ(ys), SP, idx);
+        self.save_stack(xs, idx);
+        self.save_stack(ys, idx + 1);
+        // self.save_paired_q_to_mem(ϕ(xs), ϕ(ys), SP, idx);
     }
 
     fn save_stack_result(&mut self, idx: u32) {
