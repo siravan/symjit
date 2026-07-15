8a246d2e20ec464300e3f14646ed1bdfd98760e6
Shahriar Iravanian <siravan@svtsim.com>
2026-07-14 22:03:21 -0400

    update symbolica/large example

--- examples/symbolica/large.py
+++ examples/symbolica/large.py
@@ -4,7 +4,7 @@ import numpy as np
 from symbolica import E, S
 from symjit import compile_evaluator
 
-DEPTH = 15
+DEPTH = 4
 NCOLS = 2**DEPTH
 NROWS = 107
 
@@ -17,17 +17,17 @@ def large(a, b):
         return xs[a]
     elif (round(math.log2(d)) % 2) == 0:
         n = (a + b) // 2
-        return large(a, n) + large(n, b)
+        return (large(a, n) + large(n, b))
     else:
         n = (a + b) // 2
-        return large(a, n) * large(n, b)
+        return large(a, n) / large(n, b)
 
 
 ev = large(0, NCOLS).evaluator(xs, jit_compile=False)
 f = compile_evaluator(ev, dtype="complex128", direct=False)
 
 print(f"{len(f.dumps('simd'))} bytes")
-# print(f.dumps("simd"))
+print(f.dumps("bytecode"))
 print(large(0, 16))
 
 X = np.random.rand(NROWS, NCOLS) + np.random.rand(NROWS, NCOLS) * 1j - (0.5 + 0.5j)
