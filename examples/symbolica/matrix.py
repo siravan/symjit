d8c23c4259a60db26da801a955158ee497559797
Shahriar Iravanian <shahriar@Shahriars-Mac-mini.local>
2026-07-22 22:42:58 -0400

    updating matrix test

--- examples/symbolica/matrix.py
+++ examples/symbolica/matrix.py
@@ -18,13 +18,24 @@ def mat(v):
     return Matrix.from_linear(n, n, v)
 
 def run(n):
+    A = np.random.rand(n, n) + np.random.rand(n, n) * 1j
+
+    dt_np = 0
+
+    for _ in range(N):
+        t0 = time.thread_time_ns()
+        B = np.linalg.matrix_power(A, 3)
+        t1 = time.thread_time_ns()
+        dt_np += (t1 - t0) * 1e-6 / N
+
+    B = np.linalg.matrix_power(A, 3)
+    A0 = A.reshape(1, -1)
+    B0 = B.reshape(1, -1)
+
     v = vars(n, "x")
     m = mat(v)
     u = (m * m * m)
     ev = Expression.evaluator_multiple([u[i,j].to_expression() for i in range(n) for j in range(n)], v)
-
-    A = np.random.randn(n, n) + np.random.randn(n, n) * 1j
-    B = np.matmul(A, np.matmul(A, A))
 
     dt_gen = 0
     dt_run = 0
@@ -35,15 +46,20 @@ def run(n):
         t0 = time.thread_time_ns()
         f = compile_evaluator(ev)
         t1 = time.thread_time_ns()
-        C = f.evaluate_complex(A.reshape(1, -1)).reshape(n, n)
+
+        _ = f.evaluate_complex(A0)
+
         t2 = time.thread_time_ns()
+        C = f.evaluate_complex(A0)
+        t3 = time.thread_time_ns()
+
         dt_gen += (t1 - t0) * 1e-6 / N
-        dt_run += (t2 - t1) * 1e-6 / N
+        dt_run += (t3 - t2) * 1e-6 / N
 
-    np.testing.assert_array_almost_equal(B, C)
+        np.testing.assert_array_almost_equal(B0, C)
 
-    print(f"{n}:\tgeneration in {dt_gen:.3f} ms and execution in {dt_run:.3f} ms")
+    print(f"{n}:\tgeneration in {dt_gen:.3f} ms and execution in {dt_run:.3f} ms vs {dt_np:.3f} for np")
 
 
-for i in range(2, 18):
+for i in range(2, 30):
     run(i)
