9c7efa2abd2158c0b971e4af42dc9556fbd9bf4d
Shahriar Iravanian <shahriar@Shahriars-Mac-mini.local>
2026-09-23 23:09:00 -0400

    times_i/times_neg_i for arm

--- rust/arm/complex.rs
+++ rust/arm/complex.rs
@@ -279,13 +279,13 @@ impl Generator for ArmComplexGenerator {
     }
 
     fn times_i(&mut self, dst: Reg, s1: Reg) {
-        self.emit(arm! {ext q(ϕ(dst)), q(ϕ(s1)), q(ϕ(s1)), #8});
-        self.emit(arm! {fneg d(ϕ(dst)), d(ϕ(dst))});
+        self.emit(arm! {fneg q(T1), q(ϕ(s1))});
+        self.emit(arm! {ext q(ϕ(dst)), q(T1), q(ϕ(s1)), #8});
     }
 
     fn times_neg_i(&mut self, dst: Reg, s1: Reg) {
-        self.emit(arm! {fneg d(ϕ(dst)), d(ϕ(s1))});
-        self.emit(arm! {ext q(ϕ(dst)), q(ϕ(dst)), q(ϕ(dst)), #8});
+        self.emit(arm! {fneg d(T1), d(ϕ(s1))});
+        self.emit(arm! {ext q(ϕ(dst)), q(ϕ(s1)), q(T1), #8});
     }
 
     fn root(&mut self, dst: Reg, s1: Reg) {
